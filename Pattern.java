ac7b2689325de90ad4e7fbdfb8d65999bc0fe334
Vaibhavpan02 <89473295+Vaibhavpan02@users.noreply.github.com>
2021-10-31 19:33:50 +0530

    Create Pattern.java

---
+++ Pattern.java
@@ -0,0 +1,20 @@
+public class LeftTrianglePattern  
+{    
+public static void main(String args[])   
+{         
+int i, j, row = 6;        
+for (i=0; i<row; i++)   
+{      
+for (j=2*(row-i); j>=0; j--)         
+{       
+System.out.print(" ");   
+}     
+for (j=0; j<=i; j++ )   
+{   
+//prints star      
+System.out.print("* ");   
+}   
+System.out.println();   
+}   
+}   
+} 
